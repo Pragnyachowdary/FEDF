89fabe73eeb682a87a1b646c6845b7c4b68cd038
KLU <pragnyavaleti@gmail.com>
2026-06-14 23:07:04 +0530

    Add via files

---
+++ Week 12/latest/src/components/BookingForm.jsx
@@ -0,0 +1,10 @@
+function BookingForm() {
+  return (
+    <form>
+      <h2>Book Your Ticket</h2>
+      <button type="submit">Book Ticket</button>
+    </form>
+  );
+}
+
+export default BookingForm;
